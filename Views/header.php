1708cfa32ca1a5f76d77707b946c7a0bff522887
Abrar Hussain Syed <keyblademaster1000@gmail.com>
2019-07-08 22:51:41 -0400

    Added Bootstrap to Header and Footer

--- Views/header.php
+++ Views/header.php
@@ -7,7 +7,38 @@
 
     <!-- Bootstrap CSS -->
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css" integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous">
-
+<link rel="stylesheet" href="http://ahussainsy-ed.stackstaging.com/MVC%20Twitter%20Clone/styles.css">
     <title>Hello, world!</title>
   </head>
   <body>
+
+
+
+    <nav class="navbar navbar-expand-lg navbar-light bg-light">
+      <a class="navbar-brand" href="http://ahussainsy-ed.stackstaging.com/MVC%20Twitter%20Clone/">Tweeter</a>
+      <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+        <span class="navbar-toggler-icon"></span>
+      </button>
+
+      <div class="collapse navbar-collapse" id="navbarSupportedContent">
+        <ul class="navbar-nav mr-auto">
+
+          <li class="nav-item">
+            <a class="nav-link" href="?page=timeline">Your Timeline</a>
+          </li>
+          <li class="nav-item dropdown">
+            <a class="nav-link" href="?page=yourtweets" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+              Your Tweets
+            </a>
+          </li>
+          <li class="nav-item dropdown">
+            <a class="nav-link" href="?page=publicprofiles" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+Public Profiles
+            </a>
+          </li>
+        </ul>
+        <form class="form-inline my-2 my-lg-0">
+          <button class="btn btn-outline-success my-2 my-sm-0" type="submit">Login/Sign Up</button>
+        </form>
+      </div>
+    </nav>
